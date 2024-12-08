762ab0efebcb4e8206584704ac5794cdab2e5191
dakirzakaria <dakir.03.zakaria.02@gmail.com>
2024-12-08 03:18:51 +0100

    Add modal form styles and event handling for project creation
    - Added styles for modal buttons and form elements.
    - Implemented functions to create and display the modal overlay and form.
    - Added event listener to show the project modal form on button click.

--- scripts/components.js
+++ scripts/components.js
@@ -1,1 +1,46 @@
 "use strict";
+
+export const openProjectModalForm = (formType) => {
+  createProjectModalForm(formType);
+  createModalOverlay("add");
+};
+
+const createModalOverlay = (actionType) => {
+  const modalOverlay = document.createElement("div");
+  modalOverlay.id = "modal-overlay";
+  const baseClass = "modal-overlay";
+  const opacityClass = actionType === "add" ? "opacity-50" : "opacity-10";
+  const backgroundColorClass = actionType === "add" ? "bg-black" : "bg-red-500";
+  modalOverlay.classList.add(baseClass, opacityClass, backgroundColorClass);
+  document.body.appendChild(modalOverlay);
+};
+
+const createProjectModalForm = (formType) => {
+  const form = document.createElement("form");
+  form.id = "project-modal-form";
+  form.classList.add("project-modal-form");
+  form.appendChild(createModalFormTitle(formType));
+  form.appendChild(createModalFormButtons(formType));
+  document.body.appendChild(form);
+};
+
+const createModalFormTitle = (formType) => {
+  const title = document.createElement("h2");
+  title.classList.add("modal-form-title");
+  title.textContent = formType === "add" ? "Add New Project" : "Edit Project";
+  return title;
+};
+
+const createModalFormButtons = (formType) => {
+  const container = document.createElement("div");
+  container.id = "modal-form-buttons";
+  container.classList.add("modal-form-buttons");
+  container.innerHTML = `
+  <button type="button" id="cancel-modal-button" class="button secondary-button">Cancel</button>
+  <button type="submit" id="submit-modal-button" class="button primary-button">${
+    formType === "add" ? "Add Project" : "Save Changes"
+  }</button>
+  `;
+  return container;
+};
+
--- scripts/scripts.js
+++ scripts/scripts.js
@@ -1,12 +1,35 @@
 "use strict";
 
 import { projectsArray } from "./data.js";
+import { openProjectModalForm } from "./components.js";
 
 /* DOM elements */
 const projectsCountElement = document.getElementById("projects-count");
+const projectSearchInput = document.getElementById("project-search-input");
+const createProjectButton = document.getElementById("create-project-button");
+
+/* State Variables */
+let currentSearchTerm = "";
 
 /* Functions */
 const updateProjectsCount = () => {
   projectsCountElement.textContent = projectsArray.length;
 };
 updateProjectsCount();
+
+const searchProjects = (searchTerm) => {
+  let searchedProjects = projectsArray.filter((project) =>
+    project.name.toLowerCase().includes(searchTerm)
+  );
+  console.table(searchedProjects);
+};
+
+/* Event listeners */
+projectSearchInput.addEventListener("input", () => {
+  currentSearchTerm = projectSearchInput.value.trim().toLowerCase();
+  searchProjects(currentSearchTerm);
+});
+
+createProjectButton.addEventListener("click", () => {
+  openProjectModalForm("add");
+});
