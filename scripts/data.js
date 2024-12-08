31688ff9b83f476367d8f334121c171d7c2a3802
dakirzakaria <dakir.03.zakaria.02@gmail.com>
2024-12-08 21:10:10 +0100

    Add pagination controls to HTML file and enhance secondary button styling
    - Added a pagination section to the HTML file, including controls for navigating between pages (previous and next buttons) and a dropdown for selecting rows per page.
    - Applied ARIA attributes to improve accessibility for pagination controls.
    - Enhanced the .secondary-button class in input.css with Tailwind properties for consistent styling and hover/disabled states.

--- scripts/data.js
+++ scripts/data.js
@@ -15,6 +15,72 @@ export let projectsArray = JSON.parse(
     isArchived: false,
     status: "At Risk",
   },
+  {
+    id: 3,
+    name: "Tarius landing page",
+    isArchived: true,
+    status: "On Hold",
+  },
+  {
+    id: 4,
+    name: "Rugops App",
+    isArchived: true,
+    status: "At Risk",
+  },
+  {
+    id: 5,
+    name: "Erketu",
+    isArchived: true,
+    status: "At Risk",
+  },
+  {
+    id: 6,
+    name: "Capricorn",
+    isArchived: true,
+    status: "At Risk",
+  },
+  {
+    id: 7,
+    name: "Sagittarius",
+    isArchived: true,
+    status: "At Risk",
+  },
+  {
+    id: 8,
+    name: "Gemini",
+    isArchived: true,
+    status: "At Risk",
+  },
+  {
+    id: 9,
+    name: "Pisces",
+    isArchived: true,
+    status: "At Risk",
+  },
+  {
+    id: 10,
+    name: "Taurus",
+    isArchived: true,
+    status: "At Risk",
+  },
+  {
+    id: 11,
+    name: "Osiris",
+    isArchived: false,
+    status: "On Hold",
+  },
+  {
+    id: 12,
+    name: "Horus",
+    isArchived: false,
+    status: "On Hold",
+  },
+  {
+    id: 13,
+    name: "Hathor",
+    isArchived: false,
+    status: "On Hold",
+  },
 ];
 
 export let filterTabsArray = [
